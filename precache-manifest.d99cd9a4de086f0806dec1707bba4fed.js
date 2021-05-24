f51306931f32303766d5994263d9f7fb1273921b
David Rochus <david.rochus@gmail.com>
2021-05-24 16:11:57 +0200

    Updates

---
+++ precache-manifest.d99cd9a4de086f0806dec1707bba4fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a42026cb5a52f0349216c4e453eb08",
+    "url": "/tdd-simple-storage/index.html"
+  },
+  {
+    "revision": "5aaefa3350715cf154a6",
+    "url": "/tdd-simple-storage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7ad763286d4bc20ba14c",
+    "url": "/tdd-simple-storage/static/js/2.9becbd21.chunk.js"
+  },
+  {
+    "revision": "5aaefa3350715cf154a6",
+    "url": "/tdd-simple-storage/static/js/main.07916b67.chunk.js"
+  },
+  {
+    "revision": "8e8ad18830462c839c1f",
+    "url": "/tdd-simple-storage/static/js/runtime-main.4a6f39e4.js"
+  }
+]);
